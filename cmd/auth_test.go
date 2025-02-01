19c326156e0ffaa198a7cbef022192250db68a12
vadkvadrad <vadik.kudrv@yandex.ru>
2025-02-01 21:43:32 +0300

    test login success

---
+++ cmd/auth_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"adv-go/api/internal/auth"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestLoginSuccess(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email: "va@ya.ru",
+		Password: "1234",
+	})
+
+	res, err := http.Post(ts.URL + "/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("expected %d, got %d", 200, res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("expected token, got ", resData.Token)
+	}
+}
