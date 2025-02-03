8b44cf7c8823e63cbf551508bdebf6beb83aca82
vadkvadrad <vadik.kudrv@yandex.ru>
2025-02-03 18:30:58 +0300

    auth/handler test login success

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require github.com/go-ozzo/ozzo-validation/v4 v4.3.0
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
---
+++ internal/auth/handler_test.go
@@ -0,0 +1,75 @@
+package auth_test
+
+import (
+	"adv-go/api/configs"
+	"adv-go/api/internal/auth"
+	"adv-go/api/internal/user"
+	"adv-go/api/pkg/db"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	email = "a@1a.ru"
+	password = "1234"
+)
+
+func bootstrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
+	database, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+	gormDb, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: database,
+	}))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	userRepo := user.NewUserRepository(&db.Db{
+		DB: gormDb,
+	})
+
+	handler := auth.AuthHandler{
+		Config: &configs.Config{
+			Auth: configs.AuthConfig{
+				Secret: "secret",
+			},
+		},
+		AuthService: auth.NewAuthService(userRepo),
+	}
+
+	return &handler, mock, nil
+}
+
+func TestLoginSuccess(t *testing.T) {
+	handler, mock, err := bootstrap()
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	rows := sqlmock.NewRows([]string{"email", "password"}).AddRow(
+		"a1@a.ru",
+		"$2a$10$ruTbXRDNF9.ulyWTp1weUOl8W4WLyGYI6eehUe9gY6fyO8NYhebGe",
+	)
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	data, _ :=  json.Marshal(&auth.LoginRequest{
+		Email: email,
+		Password: password,
+	})
+	reader := bytes.NewReader(data)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/auth/login", reader)
+	handler.Login()(w, req)
+
+	if w.Result().StatusCode != http.StatusOK {
+		t.Errorf("status got: %d, want %d", w.Result().StatusCode, http.StatusOK)
+	}
+}
